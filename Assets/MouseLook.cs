0b383f2dcdb832eb3c47b3040c57333738423a21
Eugene Sia <eugene@eugenesia.co.uk>
2016-03-18 03:41:20 +0000

    Add MouseLook script sect 2.4

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Allow player to look in different directions using the mouse.
+public class MouseLook : MonoBehaviour {
+
+	// Show human-readable names for the rotation setting in editor.
+	public enum RotationAxes {
+		MouseXAndY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
+	// Public var to set and serialize in Unity editor.
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			// horizontal rotation here.
+		}
+		else if (axes == RotationAxes.MouseY) {
+			// vertical rotation here.
+		}
+		else {
+			// both horizontal and vertical rotation here.
+		}
+	}
+}
