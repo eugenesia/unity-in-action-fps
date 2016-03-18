62a577dfe5720fd0bbe23bc6ba6f312c5a0d4db8
Eugene Sia <eugene@eugenesia.co.uk>
2016-03-18 04:26:47 +0000

    Create movement script FPSInput sect 2.5

---
+++ Assets/FPSInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Allow moving the player.
+public class FPSInput : MonoBehaviour {
+	// Speed of movement.
+	public float speed = 6.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, speed, 0);
+	}
+}
