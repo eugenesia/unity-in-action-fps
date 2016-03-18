38fb0dbb4f6f7a1827ccf93daa82e6bfc553761e
Eugene Sia <eugene@eugenesia.co.uk>
2016-03-18 03:26:33 +0000

    Create Spin code 2.3.2

---
+++ Assets/Spin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+	// Speed of rotation.
+	// Public variable's modifications in Inspector are serialized by Unity.
+	public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Rotate once every frame.
+		transform.Rotate(0, speed, 0);
+	}
+}
