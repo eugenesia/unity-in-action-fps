207333d9e5f1837ef9ee8dce761db33cedeac6ad
Eugene Sia <eugene@eugenesia.co.uk>
2016-04-02 02:08:46 +0100

    Use ScreenPointToRay for shooting sect 3.1.2

---
+++ Assets/RayShooter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour {
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		// Access the camera component attached to same object.
+		_camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Respond to mouse click.
+		if (Input.GetMouseButtonDown(0)) {
+			// Middle of screen is half its width and height.
+			Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
+
+			// Create a ray from camera to screen point.
+			Ray ray = _camera.ScreenPointToRay(point);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				Debug.Log("Hit " + hit.point);
+			}
+		}
+	}
+}
