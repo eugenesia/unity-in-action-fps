56973227240e5af5a7b7c59993ceedd973cb1b55
Eugene Sia <eugene@eugenesia.co.uk>
2016-04-02 01:40:56 +0100

    Move CharacterController instead of player object sect 2.5.3

--- Assets/FPSInput.cs
+++ Assets/FPSInput.cs
@@ -6,9 +6,13 @@ public class FPSInput : MonoBehaviour {
 	// Speed of movement.
 	public float speed = 6.0f;
 
+	// Reference CharacterController component.
+	private CharacterController _charController;
+
 	// Use this for initialization
 	void Start () {
-	
+		// Access other components connected to the same object.
+		_charController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -17,8 +21,16 @@ public class FPSInput : MonoBehaviour {
 		// keyboard mappings.
 		float deltaX = Input.GetAxis("Horizontal") * speed;
 		float deltaZ = Input.GetAxis("Vertical") * speed;
+		Vector3 movement = new Vector3(deltaX, 0, deltaZ);
 
-		// Multiply by deltaTime to get frame rate-independent movement.
-		transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+		// Limit diagonal movement to same speed as movement along an axis.
+		movement = Vector3.ClampMagnitude(movement, speed);
+
+		// Make movement speed independent of frame rate.
+		movement *= Time.deltaTime;
+
+		// Transform a direction from local space to world space.
+		movement = transform.TransformDirection(movement);
+		_charController.Move(movement);
 	}
 }
