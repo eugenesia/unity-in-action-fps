251520d76663a943e2f689c5597ce50fdf693be7
Eugene Sia <eugene@eugenesia.co.uk>
2016-04-20 23:21:47 +0100

    Damage the player's health sect 3.5.3

--- Assets/Fireball.cs
+++ Assets/Fireball.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Fireball shooting behavior.
 public class Fireball : MonoBehaviour {
+
+	// How fast the fireball moves.
+	public float speed = 10.0f;
+	// How much damage it causes the player.
+	public int damage = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +16,18 @@ public class Fireball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// Move forward.
+		transform.Translate(0, 0, speed * Time.deltaTime);
+	}
+
+	// Called when fireball collides, e.g. with walls or player.
+	// For this to be called, one of the objects must have a RigidBody
+	// component.
+	void OnTriggerEnter(Collider other) {
+		PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+		if (player != null) {
+			player.Hurt(damage);
+		}
+		Destroy(this.gameObject);
 	}
 }
--- Assets/PlayerCharacter.cs
+++ Assets/PlayerCharacter.cs
@@ -4,14 +4,20 @@ using System.Collections;
 // This is attached to a GameObject so other scripts can recognize this object
 // as the player.
 public class PlayerCharacter : MonoBehaviour {
+	private int _health;
 
 	// Use this for initialization
 	void Start () {
-	
+		_health = 5;
 	}
-	
+
+	// Allow other scripts to hurt the player.
+	public void Hurt(int damage) {
+		_health -= damage;
+		Debug.Log("Health: " + _health);
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
