a9daa1fdb3a0d6d2cbb9c4489636098a8dd3ed1c
Eugene Sia <eugene@eugenesia.co.uk>
2016-04-21 02:05:36 +0100

    Attach particle effects to 3D object sect 4.6.3

---
+++ Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Move an object back and forth along a straight path.
+public class BackAndForth : MonoBehaviour {
+
+	public float speed = 3.0f;
+
+	// Positions the object moves between.
+	public float maxZ = 16.0f;
+	public float minZ = -16.0f;
+
+	// Which direction object is currently moving in.
+	private int _direction = 1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+
+		bool bounced = false;
+		if (transform.position.z > maxZ || transform.position.z < minZ) {
+			_direction = -_direction;
+			bounced = true;
+		}
+
+		// Make an extra movement this frame if object switched directions.
+		if (bounced) {
+			transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+		}
+	}
+}
